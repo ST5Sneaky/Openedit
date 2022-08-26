c89c942a7ea1ec5a22075bed26eccbc3748b24d8
ST5Sneaky <105569073+ST5Sneaky@users.noreply.github.com>
2022-08-26 17:00:11 -0400

    for windows

---
+++ dependecies.bat
@@ -0,0 +1,6 @@
+echo Install FFmpeg using your browser, then click any key once FFmpeg has been installed.
+pause
+pip install pywin32
+pip install ffmpeg-python
+echo All the dependecies for Openedit have been installed on your computer.
+pause
