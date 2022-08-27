388a3722ca6944206e3111ec78f73b35b7f7b8c3
ST5Sneaky <105569073+ST5Sneaky@users.noreply.github.com>
2022-08-26 20:57:47 -0400

    TADA IM DUMB

---
+++ dependecies2.11.bat
@@ -0,0 +1,7 @@
+@echo off
+title Openedit/Python Dependencies Batch Installer Version 2.1
+echo Install FFmpeg using your browser, then click any key once FFmpeg has been installed.
+start https://ffmpeg.org/download.html
+pause
+echo After that, all the dependecies for Openedit have been installed on your computer.
+pause
